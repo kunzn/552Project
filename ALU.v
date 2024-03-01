d9136894e78262e1c413cd4f4831a3481135e32f
nkunz <nkunz@ad.engr.wisc.edu>
2024-02-29 19:55:19 -0600

    ALU and PC_control

--- ALU.v
+++ ALU.v
@@ -2,56 +2,101 @@ module ALU (
   input [15:0] ALU_In1, ALU_In2,
   input [3:0] Opcode,
   output [15:0] ALU_Out,
-  output Error // Just to show overflow
+  output [2:0] F // VNZ
 );
   reg sub;
   reg shifter_mode;
   reg inter_in2;
+  reg ALU_Return;
+  reg Adder_In_2;
 
-  wire adder_output;
-  wire red_output;
-  wire paddsb_output;
-  wire shifter_output;
-  wire paddsb_output;
+  wire [15:0] adder_output;
+  wire [15:0] red_output;
+  wire [15:0] paddsb_output;
+  wire [15:0] shifter_output;
+  wire adder_ovfl;
+  wire passdb_ovfl;
+
+  reg Z, V, N;
 
   // add: rs + rt 
-  Add_Sub_16bit adder(.A(ALU_In1), .B(ALU_In2), .sub(sub), .Sum(adder_output), .Ovfl());
+  Add_Sub_16bit adder(.A(ALU_In1), .B(Adder_In_2), .sub(sub), .Sum(adder_output), .Ovfl(adder_ovfl));
   Red reduction(.A(ALU_In1), .B(ALU_In2), .Sum(red_output));
 
   // rs + 4 bit_immediate
-  Shifter shift(.Shift_In(ALU_In1), .Shift_Val(ALU_In2[3:0]), .Mode(Opcode[1:0]), .Shift_Out(shifter_output))
-  PSA_16bit paddsb(.A(ALU_In1), .B(ALU_In2), .Sum(paddsb_output), .Error());
+  Shifter shift(.Shift_In(ALU_In1), .Shift_Val(ALU_In2[3:0]), .Mode(Opcode[1:0]), .Shift_Out(shifter_output));
+  PSA_16bit paddsb(.A(ALU_In1), .B(ALU_In2), .Sum(paddsb_output), .Error(passdb_ovfl));
+
+  assign ALU_Out = ALU_Return;
+  assign F = {V, N, Z};
 
   always @(*) 
   begin
     case(Opcode)
-    4'b0000: 
+    4'b0000: begin  // ADD
       sub = 0;
-      ALU_Out = adder_output;
-    4'b0001: 
+      Adder_In_2 = ALU_In2;
+      ALU_Return = adder_output;
+      V = adder_ovfl;
+      Z = ~(|adder_output);
+      N = adder_output[15];
+      end
+    4'b0001: begin // SUB
       sub = 1;
-      ALU_Out = adder_output;
-    4'b0010:
-      ALU_Out = ALU_In1 ^ ALU_In2;
-    4'b0011:
-      ALU_Out = red_output;
-    4'b0100:
-      ALU_Out = shifter_output;
-    4'b0101:
-      ALU_Out = shifter_output;
-    4'b0110:
-      ALU_Out = shifter_output;
-    4'b0111:
-      ALU_Out = paddsb_output;
-    4'b1000:
-    4'b1001:
-    4'b1010:
-    4'b1011:
-    4'b1100:
-    4'b1101:
-    4'b1110:
-    4'b1111:
-    default: 
-  endcase
+      Adder_In_2 = ALU_In2;
+      ALU_Return = adder_output;
+      V = adder_ovfl;
+      Z = ~(|adder_output);
+      N = adder_output[15];
+      end
+    4'b0010: begin // XOR
+      ALU_Return = ALU_In1 ^ ALU_In2;
+      Z = ~(|ALU_Return);
+      end
+    4'b0011: begin // RED
+      ALU_Return = red_output;
+      end
+    4'b0100: begin // SLL
+      ALU_Return = shifter_output;
+      Z = ~(|ALU_Return);
+      end
+    4'b0101: begin // SRA
+      ALU_Return = shifter_output;
+      Z = ~(|ALU_Return);
+      end
+    4'b0110: begin // ROR
+      ALU_Return = shifter_output;
+      end
+    4'b0111: begin // PADDSB
+      ALU_Return = paddsb_output;
+      end   
+    4'b1000: begin// LW
+      sub = 0;
+      Adder_In_2 = ALU_In2 << 1;
+      ALU_Return = adder_output;
+      end 
+    4'b1001: begin// SW
+      sub = 0;
+      Adder_In_2 = ALU_In2 << 1;
+      ALU_Return = adder_output;
+      end 
+    4'b1010: begin// LLB
+	// Nothing happens??
+      end 
+    4'b1011: begin// LHB
+	// Nothing happens??
+      end 
+    4'b1100: begin// B
+      end 
+    4'b1101: begin// BR
+      end 
+    4'b1110: begin// PCS
+      end 
+    4'b1111: begin// HLT
+      end 
+    default: begin
+      end 
+    endcase
+  end
 endmodule
 
--- PC_control.v
+++ PC_control.v
@@ -1,25 +1,14 @@
 module PC_control(C, I, F, PC_in, PC_out);
 
-input [2:0] C; //condition
-input [8:0] I; // offset, immediate and signed
-input [2:0] F; // flag
-input [15:0] PC_in;
-output[15:0] PC_out;
+  input [2:0] C; //condition
+  input [8:0] I; // offset, immediate and signed
+  input [2:0] F; // flag
+  input [15:0] PC_in;
+  output[15:0] PC_out;
 
-
-// 000 = Not Equal (Z=0)
-
-// 001 = Equal (Z=1)
-
-// 010 = Greater Than (Z = N = 0)
-
-// 011 = Less Than (N = 1)
-
-// 100 = Greater Than or Equal (Z = 1 or Z = N = 0)
-
-// 101 = Less Than or Equal
-
-
-
+  wire [15:0] PC_Add_Out;
+  wire Ovfl;
+  Add_Sub_16bit adder(.A(PC_in << 1), .B({6'b0, I, 1'b0}), .sub(1'b0), .Sum(PC_Add_Out), .Ovfl(Ovfl));
+  assign PC_out = &(C) ? PC_Add_Out : |(C ^ F) ? PC_in << 1 : PC_Add_Out;
 
 endmodule
